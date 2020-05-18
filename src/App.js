5fa37292dd495e0df636be74d48f6c6e430de900
Rahul Tripathi <rahultripathi@Rahuls-MacBook-Air.local>
2020-05-18 20:53:36 +0530

    header is created and work properly

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import {Switch,Route} from 'react-router-dom';
 import HomePage from '../src/pages/HomePage'
 import Shop from "../src/pages/shop/shop"
-
+import Header from "../src/components/Header/header"
 function App() {
   return (
     <div >
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={Shop} />
